c7ccf9b490be2f7df6f869841373fdb327840f08
cody <qudgus1984@naver.com>
2023-06-01 20:00:40 +0900

    [Refactor] Combine -> RxSwift로 전부 API 통신 변환 완료

--- SideProject-MovieApp/Sources/Domain/UseCases/AuthUseCase.swift
+++ SideProject-MovieApp/Sources/Domain/UseCases/AuthUseCase.swift
@@ -6,21 +6,21 @@
 //
 
 import Foundation
-import Combine
+import RxSwift
 
 protocol AuthUseCase {
-    func signUpExcute(user_id: String, password: String, name: String, sns_type: String, agency: String, phone_number: String, nickname: String, mbti: String) -> AnyPublisher<Int, NetworkError>
+    func signUpExcute(user_id: String, password: String, name: String, sns_type: String, agency: String, phone_number: String, nickname: String, mbti: String) -> Single<Int>
     
-    func phoneNumberCheckExcute(phone_number: String) -> AnyPublisher<PhoneNumberCheck, NetworkError>
+    func phoneNumberCheckExcute(phone_number: String) -> Single<PhoneNumberCheck>
     
-    func phoneNumberVerifyExcute(phone_number: String, code: String) -> AnyPublisher<PhoneNumberVerify, NetworkError>
+    func phoneNumberVerifyExcute(phone_number: String, code: String) -> Single<PhoneNumberVerify>
     
-    func duplicationIdExcute(user_id: String) -> AnyPublisher<DuplicationId, NetworkError>
+    func duplicationIdExcute(user_id: String) -> Single<DuplicationId>
 }
 
 final class AuthUseCaseImpl: AuthUseCase {
+
     let authRepository: AuthRepository
-    private var anyCancellable = Set<AnyCancellable>()
 
     init(authRepository: AuthRepository) {
         self.authRepository = authRepository
@@ -28,94 +28,77 @@ final class AuthUseCaseImpl: AuthUseCase {
 }
 
 extension AuthUseCaseImpl {
-    func signUpExcute(user_id: String, password: String, name: String, sns_type: String, agency: String, phone_number: String, nickname: String, mbti: String) -> AnyPublisher<Int, NetworkError> {
-        let signUpQuery = SignUpQuery(user_id: user_id, password: password, name: name, sns_type: sns_type, agency: agency, phone_number: phone_number, nickname: nickname, mbti: mbti)
-        return Future<Int, NetworkError> { [weak self] promise in
-            guard let self else { return }
-            print("서버에게 보내는 Query", signUpQuery)
-            self.authRepository.requestSignUp(query: signUpQuery).sink { completion in
-                if case .failure(let error) = completion {
-                    switch error {
-                    default:
-                        promise(.failure(error))
-                    }
+    func signUpExcute(user_id: String, password: String, name: String, sns_type: String, agency: String, phone_number: String, nickname: String, mbti: String) -> Single<Int> {
+        let query = SignUpQuery(user_id: user_id, password: password, name: name, sns_type: sns_type, agency: agency, phone_number: phone_number, nickname: nickname, mbti: mbti)
+        return Single.create { single in
+            let disposable = self.authRepository.requestSignUp(query: query).subscribe { result in
+                switch result {
+                case .success(let statusCode):
+                    single(.success(statusCode))
+                case .failure(let error):
+                    single(.failure(error))
                 }
-            } receiveValue: { statusCode in
-                print("UseCase",statusCode)
-                promise(.success(statusCode))
             }
-            .store(in: &self.anyCancellable)
+            return Disposables.create {
+                disposable.dispose()
+            }
         }
-        .eraseToAnyPublisher()
     }
 }
 
 extension AuthUseCaseImpl {
-    func phoneNumberCheckExcute(phone_number: String) -> AnyPublisher<PhoneNumberCheck, NetworkError> {
-        let phoneNumberCheckQuery = PhoneNumberCheckQuery(phone_number: phone_number)
-        return Future<PhoneNumberCheck, NetworkError> { [weak self] promise in
-            guard let self else { return }
-            print("Query", phoneNumberCheckQuery)
-            self.authRepository.requestPhoneNumberCheck(query: phoneNumberCheckQuery).sink { completion in
-                if case .failure(let error) = completion {
-                    switch error {
-                    default:
-                        promise(.failure(error))
-                    }
+    func phoneNumberCheckExcute(phone_number: String) -> Single<PhoneNumberCheck> {
+        let query = PhoneNumberCheckQuery(phone_number: phone_number)
+        return Single.create { single in
+            let disposable = self.authRepository.requestPhoneNumberCheck(query: query).subscribe { result in
+                switch result {
+                case .success(let data):
+                    single(.success(data))
+                case .failure(let error):
+                    single(.failure(error))
                 }
-            } receiveValue: { phoneNumberCheck in
-                print("응답모델", phoneNumberCheck)
-                promise(.success(phoneNumberCheck))
             }
-            .store(in: &self.anyCancellable)
+            return Disposables.create {
+                disposable.dispose()
+            }
         }
-        .eraseToAnyPublisher()
     }
 }
 
 extension AuthUseCaseImpl {
-    func phoneNumberVerifyExcute(phone_number: String, code: String) -> AnyPublisher<PhoneNumberVerify, NetworkError> {
-        let phoneNumberVerifyQuery = PhoneNumberVerifyQuery(phone_number: phone_number, code: code)
-        return Future<PhoneNumberVerify, NetworkError> { [weak self] promise in
-            guard let self else { return }
-            print("Query", phoneNumberVerifyQuery)
-            self.authRepository.requestPhoneNumberVerify(query: phoneNumberVerifyQuery).sink { completion in
-                if case .failure(let error) = completion {
-                    switch error {
-                    default:
-                        promise(.failure(error))
-                    }
+    func phoneNumberVerifyExcute(phone_number: String, code: String) -> Single<PhoneNumberVerify> {
+        let query = PhoneNumberVerifyQuery(phone_number: phone_number, code: code)
+        return Single.create { single in
+            let disposable = self.authRepository.requestPhoneNumberVerify(query: query).subscribe { result in
+                switch result {
+                case .success(let data):
+                    single(.success(data))
+                case .failure(let error):
+                    single(.failure(error))
                 }
-            } receiveValue: { phoneNumberVerify in
-                print("응답모델", phoneNumberVerify)
-                promise(.success(phoneNumberVerify))
             }
-            .store(in: &self.anyCancellable)
+            return Disposables.create {
+                disposable.dispose()
+            }
         }
-        .eraseToAnyPublisher()
     }
 }
 
 extension AuthUseCaseImpl {
-    func duplicationIdExcute(user_id: String) -> AnyPublisher<DuplicationId, NetworkError> {
-        let duplicationIdQuery = DuplicationIdQuery(user_id: user_id)
-        return Future<DuplicationId, NetworkError> { [weak self] promise in
-            guard let self else { return }
-            print("Query", duplicationIdQuery)
-            self.authRepository.requestDuplicationId(query: duplicationIdQuery).sink { completion in
-                if case .failure(let error) = completion {
-                    switch error {
-                    default:
-                        promise(.failure(error))
-                    }
+    func duplicationIdExcute(user_id: String) -> Single<DuplicationId> {
+        let query = DuplicationIdQuery(user_id: user_id)
+        return Single.create { single in
+            let disposable = self.authRepository.requestDuplicationId(query: query).subscribe { result in
+                switch result {
+                case .success(let data):
+                    single(.success(data))
+                case .failure(let error):
+                    single(.failure(error))
                 }
-            } receiveValue: { duplicationId in
-                print("응답모델", duplicationId)
-                promise(.success(duplicationId))
             }
-            .store(in: &self.anyCancellable)
+            return Disposables.create {
+                disposable.dispose()
+            }
         }
-        .eraseToAnyPublisher()
     }
 }
-
