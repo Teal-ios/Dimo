adfd9c70cbda36f40e70f13a84d681b233499ac7
cody <qudgus1984@naver.com>
2023-08-21 12:39:38 +0900

    [Add] 찜하기 API 조회 및 그에 따른 데이터 뿌려주기 완료

---
+++ SideProject-MovieApp/Sources/Data/Networks/Service/Domain/Entities/Model/Content/MostLikeChoiceMbti.swift
@@ -0,0 +1,8 @@
+//
+//  MostLikeChoiceMbti.swift
+//  SideProject-MovieApp
+//
+//  Created by 이병현 on 2023/08/21.
+//
+
+import Foundation
---
+++ SideProject-MovieApp/Sources/Data/Networks/Service/ResponseDTO/Content/ResponseMostLikeChoiceMbtiDTO.swift
@@ -0,0 +1,8 @@
+//
+//  ResponseMostLikeChoiceMbtiDTO.swift
+//  SideProject-MovieApp
+//
+//  Created by 이병현 on 2023/08/21.
+//
+
+import Foundation
