8a946ded56d6f576b427ca71fe6edbca69eba571
cody <qudgus1984@naver.com>
2023-11-26 16:05:31 +0900

    [Add] TextView 특정 문자열 굵게 적용 완료

--- SideProject-MovieApp/Sources/Presents/TabBar/SettingView/OpenSourceLicense/View/OpenSourceLicenseView.swift
+++ SideProject-MovieApp/Sources/Presents/TabBar/SettingView/OpenSourceLicense/View/OpenSourceLicenseView.swift
@@ -29,58 +29,58 @@ final class OpenSourceLicenseView: BaseView {
         """
 이 문서는 다음 오픈소스 프로젝트의 라이선스 정보를 제공합니다.
 
-## **1. Snapkit**
+1. Snapkit
 
-- GitHub 링크: [SnapKit GitHub](https://github.com/SnapKit/SnapKit)
+- GitHub 링크: https://github.com/SnapKit/SnapKit
 - 라이선스: MIT 라이선스
 
 SnapKit 프로젝트는 MIT 라이선스 하에 배포됩니다. MIT 라이선스에 따라, 이 소프트웨어를 사용, 복제, 수정, 병합, 게시, 배포, 하위 라이선스 부여 등을 포함한 모든 작업을 자유롭게 수행할 수 있습니다. 자세한 정보는 [MIT 라이선스 전문](https://github.com/SnapKit/SnapKit/blob/main/LICENSE)을 참조하십시오.
 
-## **2. Tabman**
+2. Tabman
 
-- GitHub 링크: [Tabman GitHub](https://github.com/uias/Tabman)
+- GitHub 링크: https://github.com/uias/Tabman
 - 라이선스: MIT 라이선스
 
 Tabman 프로젝트도 MIT 라이선스 하에 배포됩니다. MIT 라이선스의 자세한 내용은 [MIT 라이선스 전문](https://github.com/uias/Tabman/blob/master/LICENSE)을 확인하십시오.
 
-## **3. ReactiveX**
+3. ReactiveX
 
-- GitHub 링크: [ReactiveX GitHub](https://github.com/ReactiveX/RxSwift)
+- GitHub 링크: https://github.com/ReactiveX/RxSwift
 - 라이선스: Apache License 2.0
 
 ReactiveX (RxSwift) 프로젝트는 Apache License 2.0 하에 배포됩니다. Apache License 2.0은 상업적인 이용을 포함한 여러 가지 용도로 소프트웨어를 사용할 수 있는 자유로운 라이선스입니다. 자세한 내용은 [Apache License 2.0 전문](https://github.com/ReactiveX/RxSwift/blob/main/LICENSE)을 확인하십시오.
 
-## **4. GoogleSignIn**
+4. GoogleSignIn
 
-- 문서 링크: [Google Sign-In for iOS](https://firebase.google.com/docs/auth/ios/google-signin?hl=ko)
+- 문서 링크: https://firebase.google.com/docs/auth/ios/google-signin?hl=ko
 - 라이선스: Firebase Terms of Service
 
 GoogleSignIn은 Firebase에서 제공하는 서비스 중 하나로, Firebase의 이용 약관에 따라 사용됩니다. Firebase의 이용 약관은 Firebase 웹 사이트에서 확인하실 수 있습니다.
 
-## **5. KakaoOpenADK**
+5. KakaoOpenADK
 
-- GitHub 링크: [KakaoOpenADK GitHub](https://github.com/kakao/kakao-ios-sdk)
+- GitHub 링크: https://github.com/kakao/kakao-ios-sdk
 - 라이선스: Apache License 2.0
 
 KakaoOpenADK 프로젝트는 Apache License 2.0 하에 배포됩니다. Apache License 2.0의 자세한 내용은 [Apache License 2.0 전문](https://github.com/kakao/kakao-ios-sdk/blob/master/LICENSE)을 확인하십시오.
 
-## **6. KingFisher**
+6. KingFisher
 
 - GitHub 링크: [KingFisher GitHub](https://github.com/onevcat/Kingfisher)
 - 라이선스: MIT 라이선스
 
 KingFisher 프로젝트는 MIT 라이선스 하에 배포됩니다. MIT 라이선스에 대한 자세한 내용은 [MIT 라이선스 전문](https://github.com/onevcat/Kingfisher/blob/master/LICENSE)을 참조하십시오.
 
-## **7. Toast**
+7. Toast
 
-- GitHub 링크: [Toast GitHub](https://github.com/scalessec/Toast-Swift)
+- GitHub 링크: https://github.com/scalessec/Toast-Swift
 - 라이선스: MIT 라이선스
 
 Toast 프로젝트는 MIT 라이선스 하에 배포됩니다. MIT 라이선스에 대한 자세한 내용은 [MIT 라이선스 전문](https://github.com/scalessec/Toast-Swift/blob/master/LICENSE)을 확인하십시오.
 
-## **8. Lottie**
+8. Lottie
 
-- GitHub 링크: [Lottie GitHub](https://github.com/airbnb/lottie-ios)
+- GitHub 링크: https://github.com/airbnb/lottie-ios
 - 라이선스: Apache License 2.0
 
 Lottie 프로젝트는 Apache License 2.0 하에 배포됩니다. Apache License 2.0의 자세한 내용은 [Apache License 2.0 전문](https://github.com/airbnb/lottie-ios/blob/master/LICENSE)을 참조하십시오.
--- SideProject-MovieApp/Sources/Presents/TabBar/SettingView/PrivacyPolicy/View/PrivacyPolicyViewController.swift
+++ SideProject-MovieApp/Sources/Presents/TabBar/SettingView/PrivacyPolicy/View/PrivacyPolicyViewController.swift
@@ -19,11 +19,43 @@ final class PrivacyPolicyViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         selfView.contentLabel.delegate = self
+        updateTextViewAttribute(text: selfView.contentLabel.text, keywords:
+        [
+            "제1조(개인정보의 처리 목적)",
+            "제2조(개인정보의 처리 및 보유 기간)",
+            "제3조(만 14세 미만 아동의 개인정보 처리에 관한 사항)",
+            "제4조(개인정보의 제3자 제공에 관한 사항)",
+            "제5조(개인정보처리의 위탁에 관한 사항)",
+            "제6조(개인정보의 파기절차 및 파기방법)",
+            "제7조(휴면 회원의 개인정보 파기 등에 관한 조치)",
+            "제8조(정보주체와 법정대리인의 권리·의무 및 그 행사방법에 관한 사항)",
+            "제9조(개인정보의 안전성 확보 조치에 관한 사항)",
+            "제10조(개인정보를 자동으로 수집하는 장치의 설치·운영 및 거부 방법)",
+            "제11조(추가적인 이용·제공 판단 기준)",
+            "제12조 (회원의 권리와 의무)",
+            "제13조 (고지의 의무)",
+            "제14조 (개인정보 처리 업무)",
+            "제15조(개인정보의 열람 및 정정)",
+            "제16조(개인정보 처리방침 변경)"
+        ]
+        )
     }
 }
 
 extension PrivacyPolicyViewController: UITextViewDelegate {
     func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
         return false
+    }
+}
+
+extension PrivacyPolicyViewController {
+    func updateTextViewAttribute(text: String, keywords: [String]) {
+        let attributtedString = NSMutableAttributedString(string: text)
+        for keyword in keywords {
+            attributtedString.addAttribute(.font, value: Font.subtitle3!, range: (text as NSString).range(of: keyword))
+        }
+
+        selfView.contentLabel.attributedText = attributtedString
+        selfView.contentLabel.textColor = .black5
     }
 }
